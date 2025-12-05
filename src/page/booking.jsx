38fd24a0d9a016661b4a388b9adf5528e37c13f2
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-12-05 13:51:59 +0530

    day7

--- src/page/booking.jsx
+++ src/page/booking.jsx
@@ -1,20 +1,3 @@
-// import React from 'react'
-
-// const Booking = () => {
-//   return (
-//     <>
-//     <div className="p-8">
-//     <h2 className="text-3xl font-semibold mb-4">Bookings</h2>
-//     <div className="bg-gray-800 p-6 rounded-xl">
-//     <p className="text-gray-300">Today's arena bookings will appear here.</p>
-//     </div>
-//     </div>
-//     </>
-//   )
-// }
-
-// export default Booking;
-
 import React, { useState } from "react";
 import bgImage from "../assets/dash.webp";
 
@@ -26,7 +9,7 @@ const Booking = () => {
     time: "",
     arena: "",
   });
-
+  
   const arenas = ["Football Ground", "Cricket Arena", "Badminton Court", "E-Sports Hall", "Basketball Court"];
 
   const handleChange = (e) => {
